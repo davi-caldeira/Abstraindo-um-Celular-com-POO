4324c13b8d2f13d0ffff74077a9930c3957e9064
DaviMCaldeira <davi.caldeira@vonbraunlabs.com.br>
2023-12-09 22:36:02 -0300

    ProjetoPOO

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+var Telefone1 = new Iphone("(31) 99985-2023", "Iphone X", "23004300EMS", 4096);
+var Telefone2 = new Iphone("(31) 99985-2024", "Nokia V5", "23004300EMS", 4096*2);
+
+Telefone1.ReceberLigacao();
+Telefone1.Ligar();
+Telefone1.InstalarAplicativo("YouTube");
+
+Telefone2.ReceberLigacao();
+Telefone2.Ligar();
+Telefone2.InstalarAplicativo("YouTube");
